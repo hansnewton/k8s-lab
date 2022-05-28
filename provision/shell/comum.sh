de06d2e0ad6e32d2858b791b00d98af501a34868
hansnewton <hansnewton@gmail.com>
2022-05-28 10:21:42 -0300

    atualizacoes para debian

--- provision/shell/comum.sh
+++ provision/shell/comum.sh
@@ -1,11 +1,12 @@
-apt install curl
-
-apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt update
 
-apt install -y docker-ce
+apt install -y curl apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+
+apt update
+
+apt-get install -y docker-ce
 systemctl enable docker --now
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
@@ -13,6 +14,7 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
+
 apt update
 
 sed -i '/swap/d' /etc/fstab
